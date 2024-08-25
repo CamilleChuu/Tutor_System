d04f4888193d18246e087a64b7c6012d1952c3bb
CamilleChuu <viachu2@gmail.com>
2024-08-25 19:11:33 -0400

    register.js

--- static/js/register.js
+++ static/js/register.js
@@ -24,8 +24,8 @@ function Register() {
 
 function submitRegister(username, password, nickname) {
     $.ajax({
-        url: 'https://192.168.1.24:5001/register',
-        // url: 'https://tutor-system-app-7c5441f65344.herokuapp.com/register',
+        // url: 'https://192.168.1.24:5001/register',
+        url: 'https://tutor-system-app-7c5441f65344.herokuapp.com/register',
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({ "username": username, 'password': password, "nickname": nickname}),
@@ -40,8 +40,8 @@ function submitRegister(username, password, nickname) {
 
 function Login(username, password) {
     $.ajax({
-        url: 'https://192.168.1.24:5001/login',
-        // url: 'https://tutor-system-app-7c5441f65344.herokuapp.com/login',
+        // url: 'https://192.168.1.24:5001/login',
+        url: 'https://tutor-system-app-7c5441f65344.herokuapp.com/login',
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify({ "username": username, 'password': password}),
