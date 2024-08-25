02c18086fd7d457719ae4b0e41ffe9599c36a8a6
CamilleChuu <viachu2@gmail.com>
2024-08-25 12:18:59 -0400

    main.py

--- main.py
+++ main.py
@@ -110,6 +110,6 @@ def action_return():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
